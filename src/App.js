6e0bc24d9b5dd610e65374e9b51bc8630a306d0e
unknown <iskradaniil@mail.ru>
2024-01-26 19:09:00 +0300

    new develop

--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { jsx as _jsx } from "react/jsx-runtime";
+import { jsxs as _jsxs } from "react/jsx-runtime";
 
 export default function App() {
-	const currentYear = new Date().getFullYear();
+  const currentYear = new Date().getFullYear();
 
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<p>{currentYear}</p>
-			</header>
-		</div>
-	);
+  return _jsx("div", {
+    className: "App",
+    children: _jsxs("header", {
+      className: "App-header",
+      children: [
+        _jsx("img", {
+          src: logo,
+          className: "App-logo",
+          alt: "logo",
+        }),
+        _jsxs("p", {
+          children: [
+            "Edit ",
+            _jsx("code", {
+              children: "src/App.js",
+            }),
+            " and save to reload.",
+          ],
+        }),
+        _jsx("a", {
+          className: "App-link",
+          href: "https://reactjs.org",
+          target: "_blank",
+          rel: "noopener noreferrer",
+          children: "Learn React",
+        }),
+        _jsx("p", {
+          children: currentYear,
+        }),
+      ],
+    }),
+  });
 }
-
-// Во всём коде декларативный стиль
